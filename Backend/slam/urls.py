1fa2c452863d7e03768b4427d58897b41c489a5b
nathanmendis <nathanmendis17@gmail.com>
2026-05-28 23:49:45 +0530

    Enable media and static file serving in production

--- Backend/slam/urls.py
+++ Backend/slam/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     path('api/', include('slambooks.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+
